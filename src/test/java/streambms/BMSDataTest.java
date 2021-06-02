0f81eacc7159a42371260bfcc1c640ab7eb12ff1
RDV1KOR <RDV1KOR@BANI-C-00058.bmh.apac.bosch.com>
2021-06-02 19:04:07 +0530

    Removed duplicates

--- src/test/java/streambms/BMSDataTest.java
+++ src/test/java/streambms/BMSDataTest.java
@@ -2,6 +2,9 @@ package streambms;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class BMSDataTest
@@ -18,8 +21,9 @@ public class BMSDataTest
   @Test
   public void testBMSDataFormat()
   {
-    IBMSData bmsData = new FakeBMSData();
-    IBMSDataFormat bmsDataFormat = bmsData.getBMSData("4");
+    List<Integer> temperatureList = Arrays.asList(20, 21, 22, 23);
+    List<Integer> socList = Arrays.asList(30, 31, 32, 33);
+    IBMSDataFormat bmsDataFormat = new BMSDataFormatInMap(temperatureList, socList).formatData();
     assertEquals("{Temperature=[20, 21, 22, 23], SOC=[30, 31, 32, 33]}", bmsDataFormat.toString());
   }
 }
